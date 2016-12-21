89df5c65a24be4be2fe0b83dfeea3d16648a9de5
LockLight <locklight@locklightdemacbook-air.local>
2016-12-21 09:58:50 +0800

    Initial Commit

---
+++ 飞行棋/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  飞行棋
+//
+//  Created by locklight on 16/12/21.
+//  Copyright © 2016年 locklight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
